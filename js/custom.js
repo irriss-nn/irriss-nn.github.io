ce11708777e866b682bfe350bcd670a947d9fae9
iris <77365934+irriss-nn@users.noreply.github.com>
2024-01-08 14:37:01 -0700

    website is updated

--- js/custom.js
+++ js/custom.js
@@ -7,14 +7,33 @@
  * License: Themeforest Standard Licenses: https://themeforest.net/licenses
  */
 
-/*----------- CUSTOM JS SCRIPTS -----------*/
+document.getElementById('about-box').addEventListener('click', function() {
+    document.querySelector('a[href="#about"]').click();
+});
+
+document.getElementById('experience-box').addEventListener('click', function() {
+    document.querySelector('a[href="#experience"]').click();
+});
+
+document.getElementById('resume-box').addEventListener('click', function() {
+    window.open('downloads/Weitong_Du_Resume-.pdf', '_blank');
+});
+
+document.getElementById('project-box').addEventListener('click', function() {
+    document.querySelector('a[href="#project"]').click();
+});
+
+document.getElementById('contact-box').addEventListener('click', function() {
+    document.querySelector('a[href="#contact"]').click();
+});
+
 
 (function($) {
-  'use strict';
-  $(function() {
-    // Code here executes When the DOM is loaded...
-  });
-  $(window).on('load', function() {
-    // Code here executes When the page is loaded
-  });
+    'use strict';
+    $(function() {
+        // Code here executes When the DOM is loaded...
+    });
+    $(window).on('load', function() {
+        // Code here executes When the page is loaded
+    });
 }(jQuery));
